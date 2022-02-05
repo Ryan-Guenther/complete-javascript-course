0b24f283fa49f4b7254d3e7bc59ee6fab6e07506
Ryan Guenther <ryan.guenther@gmail.com>
2022-02-05 15:05:25 -0700

    Completed first assignment

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+let country = "Canada";
+let continent = "North America";
+let population = 38010000;
+
+console.log(`My country is ${country}.`);
+console.log(`${country} is located in ${continent}.`);
+console.log(`${country} has a pouplation of ${population}.`);
