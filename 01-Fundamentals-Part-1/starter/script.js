5ac1bcda3d8acb4152caaf7db75b69665eba5c44
Ryan Guenther <ryan.guenther@gmail.com>
2022-02-06 06:39:21 -0700

    basic operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -32,7 +32,7 @@ console.log(typeof year);
 
 console.log(typeof null);
 
-*/
+
 // Block Scoped
 let age = 30;
 age = 31;
@@ -49,3 +49,32 @@ job = 'teacher';
 // Not in current scope, is created on global object
 lastName = "Guenther";
 console.log(lastName);
+
+*/
+const year = 2022;
+
+const ageRyan = year - 1984;
+const ageLindsey = year - 1988;
+
+console.log(ageRyan, ageLindsey);
+
+console.log(ageRyan * 2, ageRyan / 2, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 x 2 x 2
+
+const firstName = 'Ryan';
+const lastName = 'Guenther';
+console.log(firstName + ' ' + lastName);
+
+// Assignment Operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+x--; // x = x - 1
+console.log(x);
+
+//Comparison Operators
+console.log(ageRyan > ageLindsey); // >, <, >=, <=
+console.log(ageLindsey >= 18);
+
