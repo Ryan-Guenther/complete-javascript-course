4439c1d3907d6fbbcb16a250cbefb5bb75efa01c
Ryan Guenther <ryan.guenther@gmail.com>
2022-02-06 06:57:19 -0700

    Completed coding challenge 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -78,7 +78,7 @@ console.log(x);
 console.log(ageRyan > ageLindsey); // >, <, >=, <=
 console.log(ageLindsey >= 18);
 
-*/
+
 
 //Operator Precedence
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
@@ -95,3 +95,51 @@ console.log(x, y);
 
 const averageAge = (ageRyan + ageLindsey) / 2;
 console.log(ageRyan, ageLindsey, averageAge);
+
+*/
+
+// Coding Challenge #1
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is 
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg 
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both 
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about 
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 
+m tall.
+*/
+let markHeight, markMass, johnHeight, johnMass, markBMI, johnBMI, markHigherBMI;
+
+markHeight = 1.69;
+markMass = 78;
+johnHeight = 1.95;
+johnMass = 92;
+
+markBMI = markMass / markHeight ** 2;
+johnBMI = johnMass / johnHeight ** 2;
+markHigherBMI = markBMI > johnBMI;
+
+console.log("Mark's BMI is: ", markBMI);
+console.log("John's BMI is: ", johnBMI);
+console.log("Mark has a higher BMI: ", markHigherBMI);
+
+markHeight = 1.88;
+markMass = 95;
+johnHeight = 1.76;
+johnMass = 85;
+
+markBMI = markMass / markHeight ** 2;
+johnBMI = johnMass / johnHeight ** 2;
+markHigherBMI = markBMI > johnBMI;
+
+console.log("Mark's BMI is: ", markBMI);
+console.log("John's BMI is: ", johnBMI);
+console.log("Mark has a higher BMI: ", markHigherBMI);
