3b978778868448c3d9aabccf221834bff46c8ba7
Ryan Guenther <ryan.guenther@gmail.com>
2022-02-06 06:22:22 -0700

    Variable typing

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,3 +1,4 @@
+/*
 let country = "Canada";
 let continent = "North America";
 let population = 38010000;
@@ -5,3 +6,4 @@ let population = 38010000;
 console.log(`My country is ${country}.`);
 console.log(`${country} is located in ${continent}.`);
 console.log(`${country} has a pouplation of ${population}.`);
+*/
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,12 +1,34 @@
+/*
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
 console.log('Jonas');
 console.log(23);
 
+//Variable name conventions
 let firstName = 'Ryan';
 
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof javascriptIsFun);
+javascriptIsFun = 23;
+console.log(typeof javascriptIsFun);
+javascriptIsFun = "true";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
 
