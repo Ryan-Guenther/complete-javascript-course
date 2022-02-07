df52896bfc22f0b9cb60754b57b8990a99e76be9
Ryan Guenther <ryan.guenther@gmail.com>
2022-02-07 07:05:31 -0700

    Coding Challenge 3

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -100,20 +100,20 @@ console.log(ageRyan, ageLindsey, averageAge);
 
 // Coding Challenge #1
 /*
-Mark and John are trying to compare their BMI (Body Mass Index), which is 
+Mark and John are trying to compare their BMI (Body Mass Index), which is
 calculated using the formula:
-BMI = mass / height ** 2 = mass / (height * height) (mass in kg 
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
 and height in meter).
 Your tasks:
 1. Store Mark's and John's mass and height in variables
-2. Calculate both their BMIs using the formula (you can even implement both 
+2. Calculate both their BMIs using the formula (you can even implement both
 versions)
-3. Create a Boolean variable 'markHigherBMI' containing information about 
+3. Create a Boolean variable 'markHigherBMI' containing information about
 whether Mark has a higher BMI than John.
 Test data:
-§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
 m tall.
-§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
 m tall.
 */
 
@@ -197,10 +197,10 @@ console.log(century);
 
 /*
 Coding Challenge #2
-Use the BMI example from Challenge #1, and the code you already wrote, and 
+Use the BMI example from Challenge #1, and the code you already wrote, and
 improve it.
 Your tasks:
-1. Print a nice output to the console, saying who has the higher BMI. The message 
+1. Print a nice output to the console, saying who has the higher BMI. The message
 is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
 2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
 BMI (28.3) is higher than John's (23.9)!"
@@ -306,6 +306,8 @@ if (favoriteNumber !== 23) console.log('Why not 23');
 
 */
 
+/*
+
 //Boolean Logic
 //AND OR & Not(!) Operators
 
@@ -332,4 +334,71 @@ if (hasDriversLicense && hasGoodVision & !isTired) {
   console.log('Sarah is able to drive!');
 } else {
   console.log('Someone else should drive!');
+}
+
+*/
+
+/*
+Coding Challenge #3
+There are two gymnastics teams, Dolphins and Koalas. They compete against each 
+other 3 times. The winner with the highest average score wins a trophy!
+Your tasks:
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition, 
+and print it to the console. Don't forget that there can be a draw, so test for that 
+as well (draw means they have the same average score)
+3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a 
+team only wins if it has a higher score than the other team, and the same time a 
+score of at least 100 points. Hint: Use a logical operator to test for minimum 
+score, as well as multiple else-if blocks �
+4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when 
+both teams have the same score and both have a score greater or equal 100 
+points. Otherwise, no team wins the trophy
+Test data:
+§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+*/
+
+//Data 1
+// const dolphinScore1 = 96;
+// const dolphinScore2 = 108;
+// const dolphinScore3 = 89;
+
+// const koalasScore1 = 88;
+// const koalasScore2 = 91;
+// const koalasScore3 = 110;
+
+//Data 2
+// const dolphinScore1 = 97;
+// const dolphinScore2 = 112;
+// const dolphinScore3 = 101;
+
+// const koalasScore1 = 109;
+// const koalasScore2 = 95;
+// const koalasScore3 = 123;
+
+//Data 3
+const dolphinScore1 = 97;
+const dolphinScore2 = 112;
+const dolphinScore3 = 101;
+
+const koalasScore1 = 109;
+const koalasScore2 = 95;
+const koalasScore3 = 106;
+
+const averageDolphins = (dolphinScore1 + dolphinScore2 + dolphinScore3) / 3
+const averageKoalas = (koalasScore1 + koalasScore2 + koalasScore3) / 3
+
+if (averageDolphins > averageKoalas && averageDolphins > 100) {
+  console.log(`Dolphin's win ${averageDolphins} to ${averageKoalas}`);
+}
+else if (averageKoalas > averageDolphins && averageKoalas > 100) {
+  console.log(`Koala's win ${averageKoalas} to ${averageDolphins}`);
+}
+else if (averageDolphins > 100 && averageDolphins === averageKoalas) {
+  console.log(`The teams had a draw with a score of ${averageKoalas}`)
+}
+else {
+  console.log('No teams scored enough points to get a trophy!');
 }
