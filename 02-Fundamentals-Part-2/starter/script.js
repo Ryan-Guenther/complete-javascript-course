402e16d971ba0470e97bbdd3749306c51e14fe23
Ryan Guenther <ryan.guenther@gmail.com>
2022-02-07 12:44:05 -0700

    Getting started with functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -2,7 +2,7 @@
 //This needs to be the first line of every script file
 //Helps avoid accidental errors, forbidding certain actions and raise errors
 
-
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -18,3 +18,30 @@ if (hasDriversLicense) {
 // const interface = 'Audio';
 // const private = 534;
 
+*/
+
+//function definiton
+//does not need to accept parameters or return parameters but could do non, either or both
+function logger() {
+  console.log('My name is Ryan');
+}
+
+//calling//running/invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  // console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const orangeJuice = fruitProcessor(0, 5);
+console.log(orangeJuice);
+
+const mixedJuice = fruitProcessor(2, 2)
+console.log(mixedJuice);
