bbd4852db77be9255a51cfac6414a05d223587d6
Ryan Guenther <ryan.guenther@gmail.com>
2022-02-07 12:35:52 -0700

    Activating Strict Mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,20 @@
+'use strict';
+//This needs to be the first line of every script file
+//Helps avoid accidental errors, forbidding certain actions and raise errors
+
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) {
+  hasDriversLicense = true;
+}
+
+if (hasDriversLicense) {
+  console.log('I can drive.');
+}
+
+//strict mode reserves words automatically
+// const interface = 'Audio';
+// const private = 534;
+
