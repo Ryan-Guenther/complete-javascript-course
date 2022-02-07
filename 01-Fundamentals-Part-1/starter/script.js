cc939ffb93a2d62855506239783b22b6288efcb6
Ryan Guenther <ryan.guenther@gmail.com>
2022-02-06 17:55:08 -0700

    If/Else Statements

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -148,6 +148,9 @@ console.log("Mark has a higher BMI: ", markHigherBMI);
 
 */
 
+
+/*
+
 const firstName = 'Ryan';
 const job = 'Developer';
 const birthYear = 1984;
@@ -166,3 +169,26 @@ lines');
 console.log(`String with
 multiple
 lines!`);
+
+*/
+
+const age = 15;
+
+if (age >= 18) {
+  console.log('Sarah can start driving license.');
+}
+else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah needs to wait another ${yearsLeft} years to get her drivers license!`);
+}
+
+const birthYear = 1991;
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+}
+else {
+  century = 21;
+}
+
+console.log(century);
