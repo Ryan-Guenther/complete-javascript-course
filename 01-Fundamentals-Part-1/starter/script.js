b3a06b1c1f31e55c0f2f3c63a84a1f1036158674
Ryan Guenther <ryan.guenther@gmail.com>
2022-02-07 12:06:19 -0700

    expressions and statements

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -340,19 +340,19 @@ if (hasDriversLicense && hasGoodVision & !isTired) {
 
 /*
 Coding Challenge #3
-There are two gymnastics teams, Dolphins and Koalas. They compete against each 
+There are two gymnastics teams, Dolphins and Koalas. They compete against each
 other 3 times. The winner with the highest average score wins a trophy!
 Your tasks:
 1. Calculate the average score for each team, using the test data below
-2. Compare the team's average scores to determine the winner of the competition, 
-and print it to the console. Don't forget that there can be a draw, so test for that 
+2. Compare the team's average scores to determine the winner of the competition,
+and print it to the console. Don't forget that there can be a draw, so test for that
 as well (draw means they have the same average score)
-3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a 
-team only wins if it has a higher score than the other team, and the same time a 
-score of at least 100 points. Hint: Use a logical operator to test for minimum 
+3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+team only wins if it has a higher score than the other team, and the same time a
+score of at least 100 points. Hint: Use a logical operator to test for minimum
 score, as well as multiple else-if blocks �
-4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when 
-both teams have the same score and both have a score greater or equal 100 
+4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+both teams have the same score and both have a score greater or equal 100
 points. Otherwise, no team wins the trophy
 Test data:
 § Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
@@ -407,6 +407,7 @@ else {
 
 */
 
+/*
 const day = prompt('Specify the day of the week (monday, tuesday, wednesday...)');
 
 switch (day) {
@@ -448,3 +449,21 @@ if (day === 'monday') {
 } else {
   console.log('Not a valid day!');
 }
+
+*/
+
+//Expressions express values
+3 + 4
+1971
+true && false && !false
+
+//Statements is a bigger piece of code that is executed and does not produce a value on its own
+//Statements may include expression
+//Statements are like sentances that perform actions
+if (23 > 10) { const str = '23 is bigger'; }
+
+//For instance in here we can have an expression within the ${} but you couldn't put an if statement in there as that is not an expression
+console.log(`I'm ${2037 - 1991} years old.`);
+
+
+
