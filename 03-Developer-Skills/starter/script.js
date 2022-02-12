921ede0f73a0b8af124bb55339b063f3f62b8d8d
Ryan Guenther <ryan.guenther@gmail.com>
2022-02-12 16:06:49 -0700

    Developer Skills Coding Challenge 1

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -99,6 +99,8 @@ console.log(`The temperater amplitude is ${calcTempAmplitudeNew(t1, t2)}`);
 
 */
 
+/*
+
 const measureKelvin = function () {
   const measurement = {
     type: 'temp',
@@ -115,3 +117,45 @@ const measureKelvin = function () {
 };
 
 console.log(measureKelvin());
+
+*/
+
+/*
+Coding Challenge #1
+
+Given an array of forecasted maximum temperatures, the thermometer displays a 
+string with the given temperatures. Example: [17, 21, 23] will print "... 17ºC in 1 
+days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Your tasks:
+1. Create a function 'printForecast' which takes in an array 'arr' and logs a 
+string like the above to the console. Try it with both test datasets.
+2. Use the problem-solving framework: Understand the problem and break it up 
+into sub-problems!
+Test data:
+§ Data 1: [17, 21, 23]
+§ Data 2: [12, 5, -5, 0, 4]
+GOOD LUCK
+*/
+
+const printForecast = function (arr) {
+  // Declare a string to log as output
+  let message = '';
+
+  // Iterate through the array to prepare the message
+  for (let i = 0; i < arr.length; i++) {
+    message += `... ${arr[i]}ºC in ${i + 1} days `;
+  }
+
+  // append trailing ...
+  message += '...';
+
+  //Logs a string like the above function
+  console.log(message);
+};
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+printForecast(data1);
+printForecast(data2);
