b170ae61d01fb1328c078be530a8d9529ffa0e1e
Ryan Guenther <ryan.guenther@gmail.com>
2022-02-12 06:19:14 -0700

    Editor Configuration

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,17 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = '23';
+
+const calcAge = birthYear => 237 - birthYear;
+
+// installs live-server globally on the pc
+// npm install live-server -g
+
+console.log('test');
+
+// can now start live-server from terminal with live-server.ps1
+// Change your user execution policy
+// Set-Executionpolicy -Scope CurrentUser -ExecutionPolicy UnRestricted
+
+console.log(calcAge(x));
