ffa52ec2d98e0601aa13ba131dcb1aef22294d44
Ryan Guenther <ryan.guenther@gmail.com>
2022-02-14 06:40:18 -0700

    Selecting and manipulating elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,3 +1,29 @@
 'use strict';
 // because this is a class we area acessing we use '.', use '#' for id
 console.log(document.querySelector('.message').textContent);
+
+/*
+
+What is the DOM and DOM Manipulation
+  Document Object Model
+  Allows Javascript to access HTML elements/styles in order to manipulate them
+
+*/
+
+/*
+
+Selecting and manipulating elements
+
+*/
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = '13';
+document.querySelector('.score').textContent = '10';
+
+console.log(document.querySelector('.number').textContent);
+console.log(document.querySelector('.score').textContent);
+
+// On inputs we use the value property
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
