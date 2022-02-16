fd6ea0d774a22bd3c0f4422eb51edc85af531507
Ryan Guenther <ryan.guenther@gmail.com>
2022-02-16 06:13:40 -0700

    Getting Started with Modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// Store the results of the selector into a variable for reuse
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+
+console.log(modal);
+console.log(overlay);
+console.log(btnCloseModal);
+console.log(btnShowModal);
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  console.log(btnShowModal[i].textContent);
+}
