25746b105da558b347bc7b12bd1a41590cf49328
Ryan Guenther <ryan.guenther@gmail.com>
2022-02-18 18:23:20 -0700

    Definition breakdown

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,39 @@
 'use strict';
+
+/*
+
+JavaScript Definition breakdown
+
+*/
+
+// High Level Language
+// Do not need to manage resources as there are abstractions
+// These are not as optimized as low level
+
+// Garbage Collection
+// CLeans up memory automatically
+
+// Interpreted or just in time compiled
+// Conversion happens in the javascript engine
+
+// Multi-paradigm
+// Paradigm - approach/mindset of structuring code
+// Procedural, Object Oriented, or Functional
+
+// Prototype-based object-oriented
+// Everything is an object, arryas are created from array template(prototype) which has all the blueprint methods
+
+// First-class functions
+// FUnctions are treated as varriables, can pass in functions and return them from functions
+
+// Dynamically typed language
+// No data type definitions, data types on variables can be changed on the fly
+// Look into typescript for javascript with types
+
+// Single-threaded
+// Non-blocking event loop
+
+// Concurrency model
+// how javascirpt handles multiple tasks happening at the same time
+// Since javascript runs ina  single thread it can only do one thing at a time
+// By using an event loop and runnign long running tasks in the background and pushes them back on the main thread when they are finished
