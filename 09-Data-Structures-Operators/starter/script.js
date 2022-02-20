cc73c755c7f2aaaf34b84ad184f8fa4dd8b12bbb
Ryan Guenther <ryan.guenther@gmail.com>
2022-02-20 16:23:58 -0700

    Maps: Iteration

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -65,9 +65,55 @@ console.log(restaurant);
 
 /*
 ///////////////////////////////////////////////////////////
-Maps: Fundamentals
+Maps: Iteration
 ///////////////////////////////////////////////////////////
 */
+
+// can create a map without a set
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct🎉'],
+  [false, 'Try Again 👎'],
+]);
+console.log(question);
+
+// Convert object to map easkly using Object.entries
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// can iterate through maps
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key == 'number') console.log(`Answer ${key}: ${value}`);
+}
+// const answer = Number(prompt('Your Answer'));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+// You can convert a map back to an array
+console.log([...question]);
+console.log([...question.entries()]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+/*
+///////////////////////////////////////////////////////////
+Maps: Iteration
+///////////////////////////////////////////////////////////
+*/
+
+/*
+///////////////////////////////////////////////////////////
+Maps: Fundamentals
+///////////////////////////////////////////////////////////
+
 
 // Create an empty map
 const rest = new Map();
@@ -116,7 +162,7 @@ rest.set(document.querySelector('h1'), 'Heading');
 
 console.log(rest);
 
-/*
+
 ///////////////////////////////////////////////////////////
 Maps: Fundamentals
 ///////////////////////////////////////////////////////////
