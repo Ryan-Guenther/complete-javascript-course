ba8ef4023e20087760981a4be1f4f2ae87b9bf4f
Ryan Guenther <ryan.guenther@gmail.com>
2022-02-19 17:40:14 -0700

    Destructuring arrays

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,50 @@ const restaurant = {
     },
   },
 };
+
+// Destructuring is a ES6 feature to break down into smaller structures (variables)
+
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+
+// const [x, y, z] = arr;
+
+// console.log(a, b, c);
+// console.log(x, y, z);
+// console.log(arr);
+
+// You can skip or only take partial array when destructuring
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// Switching variables traditional method
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+
+// Switching variables using destructuring
+// No intermediate variable needed
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// Lets order - Receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+// Nested Destructuring
+const nested = [2, 4, [5, 6]];
+
+// destruct two values one variable (i) and one for an array (j)
+// const [i, , j] = nested;
+// console.log(i, j);
+
+//instead we can destructure in destructure
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// If we don't know the length of the array
+// We can specify default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
