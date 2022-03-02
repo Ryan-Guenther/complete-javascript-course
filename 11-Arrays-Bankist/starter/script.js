3a3fa23d176bdb852bfd0d3565e920e383db737d
Ryan Guenther <ryan.guenther@gmail.com>
2022-03-02 06:06:53 -0700

    Refactor average calculator

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -156,13 +156,21 @@ const calcAverageHumanAge = function (ages) {
   const adultAges = humanAges.filter(age => age >= 18);
   console.log(adultAges);
 
-  const averageAdultAge =
-    adultAges.reduce((acc, age) => acc + age, 0) / adultAges.length;
-  console.log(averageAdultAge);
+  // const averageAdultAge =
+  //   adultAges.reduce((acc, age) => acc + age, 0) / adultAges.length;
+
+  const averageAdultAge = adultAges.reduce(
+    (acc, age, _, arr) => acc + age / arr.length,
+    0
+  );
+
+  return averageAdultAge;
 };
 
-calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
-calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+console.log(avg1, avg2);
 
 /*
 -------------------------------------------------
